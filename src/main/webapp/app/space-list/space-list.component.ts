279415b2f9ad2e9f041ac77ddff5067f18f94894
ahmad <ahmadn3na3@gmail.com>
2025-10-22 12:57:41 +0300

    commit code

---
+++ src/main/webapp/app/space-list/space-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-space-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './space-list.component.html',
+  styleUrl: './space-list.component.css'
+})
+export class SpaceListComponent {
+
+}
